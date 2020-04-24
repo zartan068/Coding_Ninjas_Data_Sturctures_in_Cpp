2e174c7d6d7e081fd38578e0152efe5aa5ce0cef
Ashar Ansari <52534529+ashhar001@users.noreply.github.com>
2020-04-24 19:48:26 +0530

    Update MidPointLL.cpp

--- Linked List 2/MidPointLL.cpp
+++ Linked List 2/MidPointLL.cpp
@@ -1,45 +1,32 @@
-Check Case
-Send Feedback
-Write a program that takes a character as input and prints either 1, 0 or -1 according to the following rules.
-1, if the character is an uppercase alphabet (A - Z)
-0, if the character is a lowercase alphabet (a - z)
--1, if the character is not an alphabet
-Input format :
-Single Character
-Output format :
-1 or 0 or -1
-Constraints :
-Input can be any character
+Code: Midpoint of LL
+Given a linked list, find and return the midpoint.
+If the length of linked list is even, return the first mid point.
+Input format : Linked list elements (separated by space and terminated by -1)`
+
 Sample Input 1 :
-v
+1 2 3 4 5 -1
 Sample Output 1 :
-0
+3
 Sample Input 2 :
-V
+1 2 3 4 -1
 Sample Output 2 :
-1
-Sample Input 3 :
-#
-Sample Output 3 :
--1
-
-
-/****************************************** SOLUTION *************************************************************************************/
-
-#include<iostream>
-using namespace std;
-int main() {
-	// Write your code here
-	char a;
-    cin>> a;
+2
+	
+	
+/***************************************************** SOLUTION **********************************************************************/
+	
+	
+// for odd and even length
+node* midpoint_linkedlist(node *head)
+{
+    node *slow = head;
+    node *fast = head -> next;
     
-    if(a >='a' && a <='z'){
-        cout<<"0";
+    if(head != NULL){
+        while(fast != NULL && fast -> next != NULL){
+        slow = slow -> next;
+        fast = fast -> next -> next;
     }
-    else if(a >= 'A' && a <= 'Z'){
-        cout<<"1";
-    }
-    else{
-        cout<<"-1";
+    return slow;
     }
 }
