15669aeb48406dd6c91ae3109ecce679d0557242
Ashar Ansari <52534529+ashhar001@users.noreply.github.com>
2020-04-24 15:43:29 +0530

    Update ComplexNumClass.cpp

--- OOOPS 1/ComplexNumClass.cpp
+++ OOOPS 1/ComplexNumClass.cpp
@@ -1,45 +1,100 @@
-Check Case
-Send Feedback
-Write a program that takes a character as input and prints either 1, 0 or -1 according to the following rules.
-1, if the character is an uppercase alphabet (A - Z)
-0, if the character is a lowercase alphabet (a - z)
--1, if the character is not an alphabet
-Input format :
-Single Character
-Output format :
-1 or 0 or -1
-Constraints :
-Input can be any character
+Complex Number Class
+A ComplexNumber class contains two data members : one is real part (R) and other is imaginary (I) (both integer).
+Implement the Complex numbers class that contains following functions -
+1. constructor
+You need to create the appropriate constructor.
+2. plus -
+This function adds two given complex numbers and updates the first complex number.
+e.g.
+if C1 = 4 + i5 and C2 = 3 +i1
+C1.plus(C2) results in: 
+C1 = 7 + i6 and C2 = 3 + i1
+3. multiply -
+This function multiplies two given complex numbers and updates the first complex number.
+e.g.
+C1 = 4 + i5 and C2 = 1 + i2
+C1.multiply(C2) results in: 
+C1 = -6 + i13 and C2 = 1 + i2
+4. print -
+This function prints the given complex number in the following format :
+a + ib
+Note : There is space before and after '+' (plus sign) and no space between 'i' (iota symbol) and b.
+Input Format :
+Line 1 : Two integers - real and imaginary part of 1st complex number
+Line 2 : Two integers - real and imaginary part of 2nd complex number
+Line 3 : An integer representing choice (1 or 2) (1 represents plus function will be called and 2 represents multiply function will be called)
 Sample Input 1 :
-v
-Sample Output 1 :
-0
-Sample Input 2 :
-V
-Sample Output 2 :
+4 5
+6 7
 1
-Sample Input 3 :
-#
-Sample Output 3 :
--1
+Sample Output 1 :
+10 + i12
+Sample Input 2 :
+4 5
+6 7
+2
+Sample Output 2 :
+-11 + i58
 
 
-/****************************************** SOLUTION *************************************************************************************/
+/***************************************************** SOLUTION **************************************************************************/
 
-#include<iostream>
-using namespace std;
+
+/**********
+ 
+Following is the main function we are using internally.
+Refer this for completing the ComplexNumbers class.
+ 
+ 
 int main() {
-	// Write your code here
-	char a;
-    cin>> a;
+    int real1, imaginary1, real2, imaginary2;
     
-    if(a >='a' && a <='z'){
-        cout<<"0";
+    cin >> real1 >> imaginary1;
+    cin >> real2 >> imaginary2;
+    
+    ComplexNumbers c1(real1, imaginary1);
+    ComplexNumbers c2(real2, imaginary2);
+    
+    int choice;
+    cin >> choice;
+    
+    if(choice == 1) {
+        c1.plus(c2);
+        c1.print();
     }
-    else if(a >= 'A' && a <= 'Z'){
-        cout<<"1";
+    else if(choice == 2) {
+        c1.multiply(c2);
+        c1.print();
     }
-    else{
-        cout<<"-1";
+    else {
+        return 0;
     }
+    
 }
+ 
+ ************/
+
+
+class ComplexNumbers {
+    // Complete this class
+    private:
+        int real, imaginary;
+    public:
+    ComplexNumbers(int real, int imaginary){
+        this->real = real;
+        this-> imaginary = imaginary;
+    }
+    void print(){
+        cout<<real<<" + i"<<imaginary;
+    }
+    void plus(const ComplexNumbers &c2){
+        real = real + c2.real;
+        imaginary = imaginary +c2.imaginary;
+    }
+    void multiply(const ComplexNumbers c2){
+        int r = (real * c2.real)-(imaginary * c2.imaginary);
+        imaginary = (real*c2.imaginary)+(imaginary * c2.real);
+        real = r;
+    }
+    
+};
