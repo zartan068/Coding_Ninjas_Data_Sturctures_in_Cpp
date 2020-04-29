b589f5746e07f50be83e1f2cb978fc82b0baa7f1
Ashar Ansari <52534529+ashhar001@users.noreply.github.com>
2020-04-29 17:54:37 +0530

    Update NextNumber.cpp

--- Test 2/NextNumber.cpp
+++ Test 2/NextNumber.cpp
@@ -1,45 +1,47 @@
-Check Case
-Send Feedback
-Write a program that takes a character as input and prints either 1, 0 or -1 according to the following rules.
-1, if the character is an uppercase alphabet (A - Z)
-0, if the character is a lowercase alphabet (a - z)
--1, if the character is not an alphabet
+Next Number
+Given a large number represented in the form of a linked list. Write code to increment the number by 1 in-place(i.e. without using extra space).
+Note: You don't need to print the elements, just update the elements and return the head of updated LL.
+Input Constraints:
+1 <= Length of Linked List <=10^6.
 Input format :
-Single Character
-Output format :
-1 or 0 or -1
-Constraints :
-Input can be any character
+Line 1 : Linked list elements (separated by space and terminated by -1)
+Output Format :
+Line 1: Updated linked list elements 
 Sample Input 1 :
-v
+3 9 2 5 -1
 Sample Output 1 :
-0
+3 9 2 6
 Sample Input 2 :
-V
-Sample Output 2 :
-1
-Sample Input 3 :
-#
-Sample Output 3 :
--1
+9 9 9 -1
+Sample Output 1 :
+1 0 0 0 
 
-
-/****************************************** SOLUTION *************************************************************************************/
-
-#include<iostream>
-using namespace std;
-int main() {
-	// Write your code here
-	char a;
-    cin>> a;
+	
+/************************************************ SOLUTION *************************************************************************/
+	
+	
+Node* helper(Node *head){
+    if(head -> next == NULL){
+        head -> data++;
+        return head;
+    }
+    head -> next = helper(head -> next);
+    if(head->next->data > 9)
+    {
+        head->next->data=0;
+        head->data+=1;
+    }
+    return head;
+}
+Node* NextLargeNumber(Node *head) {
+   head = helper(head);
     
-    if(a >='a' && a <='z'){
-        cout<<"0";
+    if(head -> data > 9)      
+    {
+        Node *temp = new Node(1);
+        head -> data = 0;
+        temp -> next = head;
+        head = temp;
     }
-    else if(a >= 'A' && a <= 'Z'){
-        cout<<"1";
-    }
-    else{
-        cout<<"-1";
-    }
+    return head;
 }
