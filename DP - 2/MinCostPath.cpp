60b3389b24a7c10cd7980300b5e801cd40ec8358
Ashar Ansari <52534529+ashhar001@users.noreply.github.com>
2020-05-07 16:40:06 +0530

    Update MinCostPath.cpp

--- DP - 2/MinCostPath.cpp
+++ DP - 2/MinCostPath.cpp
@@ -1,45 +1,50 @@
-Check Case
-Send Feedback
-Write a program that takes a character as input and prints either 1, 0 or -1 according to the following rules.
-1, if the character is an uppercase alphabet (A - Z)
-0, if the character is a lowercase alphabet (a - z)
--1, if the character is not an alphabet
-Input format :
-Single Character
-Output format :
-1 or 0 or -1
+Code : Min Cost Path
+Given an integer matrix of size m*n, you need to find out the value of minimum cost to reach from the cell (0, 0) to (m-1, n-1).
+From a cell (i, j), you can move in three directions : (i+1, j), (i, j+1) and (i+1, j+1).
+Cost of a path is defined as the sum of values of each cell through which path passes.
+Input Format :
+Line 1 : Two integers, m and n
+Next m lines : n integers of each row (separated by space)
+Output Format :
+Minimum cost
 Constraints :
-Input can be any character
+1 <= m, n <= 20
 Sample Input 1 :
-v
+3 4
+3 4 1 2
+2 1 8 9
+4 7 8 1
 Sample Output 1 :
-0
-Sample Input 2 :
-V
-Sample Output 2 :
-1
-Sample Input 3 :
-#
-Sample Output 3 :
--1
-
-
-/****************************************** SOLUTION *************************************************************************************/
-
-#include<iostream>
+13
+	
+	
+/******************************************************* SOLUTION ************************************************************************/
+	
+	
+#include <iostream>
+#include <vector>
+#include <climits>
 using namespace std;
-int main() {
-	// Write your code here
-	char a;
-    cin>> a;
-    
-    if(a >='a' && a <='z'){
-        cout<<"0";
-    }
-    else if(a >= 'A' && a <= 'Z'){
-        cout<<"1";
-    }
-    else{
-        cout<<"-1";
-    }
+
+int minCostPath(int **input, int m, int n, int i, int j) {
+	// Base case
+	if(i == m- 1 && j == n- 1) {
+		return input[i][j];
+	}
+
+	if(i >= m || j >= n) {
+		return INT_MAX;
+	}
+
+	// Recursive calls
+	int x = minCostPath(input, m, n, i, j+1);
+	int y = minCostPath(input, m, n, i+1, j+1);
+	int z = minCostPath(input, m, n, i+1, j);
+
+	// Small Calculation
+	int ans = min(x, min(y, z)) + input[i][j];
+	return ans;
+}
+int minCostPath(int **input, int m, int n) {
+    return minCostPath(input, m, n, 0, 0);    
 }
